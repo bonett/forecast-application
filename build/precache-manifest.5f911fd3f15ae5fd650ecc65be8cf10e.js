a2fc284b8b1b9a0e85fd131a900b0429b8dbf420
Wilfrido Bonett <wbonett10@gmail.com>
2019-06-02 13:34:25 -0500

    new release and build

---
+++ build/precache-manifest.5f911fd3f15ae5fd650ecc65be8cf10e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2ec1d9144179f1d636bfd75479b743",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "d99f42e4988f40f8f172",
+    "url": "/weather-app/static/css/main.9489aae3.chunk.css"
+  },
+  {
+    "revision": "425ddad301b87a57b1b1",
+    "url": "/weather-app/static/js/2.9826182a.chunk.js"
+  },
+  {
+    "revision": "d99f42e4988f40f8f172",
+    "url": "/weather-app/static/js/main.bfae49d3.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
