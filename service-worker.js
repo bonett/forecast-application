5af726a072cf07923280bb3b644db031124d291e
Wilfrido Bonett <wbonett10@gmail.com>
2019-06-01 20:28:48 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather-app/precache-manifest.cb73c85de358a4e42e2f675b1c2899f5.js"
+  "/weather-app/precache-manifest.5989be566fceb1670711c6861c38db39.js"
 );
 
 self.addEventListener('message', (event) => {
