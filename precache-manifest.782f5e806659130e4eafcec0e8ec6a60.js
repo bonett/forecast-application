bc8b742531a7df68ebb154cffb01be993ab23887
Wilfrido Bonett <wbonett10@gmail.com>
2019-06-02 13:42:41 -0500

    Updates

--- precache-manifest.782f5e806659130e4eafcec0e8ec6a60.js
+++ precache-manifest.782f5e806659130e4eafcec0e8ec6a60.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6a2ec1d9144179f1d636bfd75479b743",
+    "revision": "88253780499af153775b5e1903a6debd",
     "url": "/weather-app/index.html"
   },
   {
-    "revision": "d99f42e4988f40f8f172",
+    "revision": "edd8f2570ecfaf647e20",
     "url": "/weather-app/static/css/main.9489aae3.chunk.css"
   },
   {
-    "revision": "425ddad301b87a57b1b1",
-    "url": "/weather-app/static/js/2.9826182a.chunk.js"
+    "revision": "a9648591452ee289532d",
+    "url": "/weather-app/static/js/2.4a494c6d.chunk.js"
   },
   {
-    "revision": "d99f42e4988f40f8f172",
-    "url": "/weather-app/static/js/main.bfae49d3.chunk.js"
+    "revision": "edd8f2570ecfaf647e20",
+    "url": "/weather-app/static/js/main.fbd7022a.chunk.js"
   },
   {
     "revision": "90e6a6c358468ae00209",
